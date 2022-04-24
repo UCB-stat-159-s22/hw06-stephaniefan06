8806e1252867313ed5cd10f0d7ed87cb6fb9b633
Yiping Fan <stephanie06@berkeley.edu>
2022-04-24 02:19:29 +0000

    Make new plotting utility in utils.py

---
+++ ligotools/ligotools/utils/utils.py
@@ -0,0 +1,136 @@
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.interpolate import interp1d
+from scipy.signal import butter, filtfilt, iirdesign, zpk2tf, freqz
+
+# This function "whitens" the noises that have high and low frequencies
+# by dividing it by the noise amplitude spectrum, in the fourier domain, 
+# suppressing the extra noise at low frequencies and at the spectral lines, 
+# to better see the weak signals in the most sensitive band.
+
+def whiten(strain, interp_psd, dt):
+	
+	# This function takes in the strain, power spectral densities(PSDs),
+	# and the time sample interval(dt).
+	# It returns the whitened sound data.
+	
+    Nt = len(strain)
+    freqs = np.fft.rfftfreq(Nt, dt)
+    freqs1 = np.linspace(0,2048.,Nt/2+1)
+
+    # whitening: transform to freq domain, divide by asd, then transform back, 
+    # taking care to get normalization right.
+    hf = np.fft.rfft(strain)
+    norm = 1./np.sqrt(1./(dt*2))
+    white_hf = hf / np.sqrt(interp_psd(freqs)) * norm
+    white_ht = np.fft.irfft(white_hf, n=Nt)
+    return white_ht
+
+
+# function to keep the data within integer limits, 
+# and write to wavfile in the audio directory
+
+def write_wavfile(filename,fs,data):
+	
+	# This function takes in the filename selected for to-save data,
+	# sampling rate, and the data
+	# It saves the wav file into the audo directory.
+	
+    d = np.int16(data/np.max(np.abs(data)) * 32767 * 0.9)
+    wavfile.write("audio/"+filename,int(fs), d)
+	
+	
+	
+# function that shifts frequency of a band-passed signal,
+# The resulting sound file will be noticibly more high-pitched, 
+# and the signal will be easier to hear.
+def reqshift(data,fshift=100,sample_rate=4096):
+	
+    # This function takes in the signal data,
+    # shifts it by frequency=100 and sample_rate=4096.
+    # It returns the shifted signal data.
+
+    x = np.fft.rfft(data)
+    T = len(data)/float(sample_rate)
+    df = 1.0/T
+    nbins = int(fshift/df)
+    # print T,df,nbins,x.real.shape
+    y = np.roll(x.real,nbins) + 1j*np.roll(x.imag,nbins)
+    y[0:nbins]=0.
+    z = np.fft.irfft(y)
+    return z
+
+
+
+
+# Plot the results
+def plot_detectors(det, time, timemax, SNR, pcolor, tevent, data, strain_whitenbp, template_match, template_fft, datafreq, d_eff, freqs, data_psd):
+    
+    fs = 4096
+    eventname = 'GW150914'
+    plottype = "png"
+    
+
+    # -- Plot the result
+    plt.figure(figsize=(10,8))
+    plt.subplot(2,1,1)
+    plt.plot(time-timemax, SNR, pcolor,label=det+' SNR(t)')
+    #plt.ylim([0,25.])
+    plt.grid('on')
+    plt.ylabel('SNR')
+    plt.xlabel('Time since {0:.4f}'.format(timemax))
+    plt.legend(loc='upper left')
+    plt.title(det+' matched filter SNR around event')
+
+    # zoom in
+    plt.subplot(2,1,2)
+    plt.plot(time-timemax, SNR, pcolor,label=det+' SNR(t)')
+    plt.grid('on')
+    plt.ylabel('SNR')
+    plt.xlim([-0.15,0.05])
+    #plt.xlim([-0.3,+0.3])
+    plt.grid('on')
+    plt.xlabel('Time since {0:.4f}'.format(timemax))
+    plt.legend(loc='upper left')
+    plt.savefig('figures/'+eventname+"_"+det+"_SNR."+plottype)
+
+    plt.figure(figsize=(10,8))
+    plt.subplot(2,1,1)
+    plt.plot(time-tevent,strain_whitenbp,pcolor,label=det+' whitened h(t)')
+    plt.plot(time-tevent,template_match,'k',label='Template(t)')
+    plt.ylim([-10,10])
+    plt.xlim([-0.15,0.05])
+    plt.grid('on')
+    plt.xlabel('Time since {0:.4f}'.format(timemax))
+    plt.ylabel('whitened strain (units of noise stdev)')
+    plt.legend(loc='upper left')
+    plt.title(det+' whitened data around event')
+
+    plt.subplot(2,1,2)
+    plt.plot(time-tevent,strain_whitenbp-template_match,pcolor,label=det+' resid')
+    plt.ylim([-10,10])
+    plt.xlim([-0.15,0.05])
+    plt.grid('on')
+    plt.xlabel('Time since {0:.4f}'.format(timemax))
+    plt.ylabel('whitened strain (units of noise stdev)')
+    plt.legend(loc='upper left')
+    plt.title(det+' Residual whitened data after subtracting template around event')
+    plt.savefig('figures/'+eventname+"_"+det+"_matchtime."+plottype)
+
+    # -- Display PSD and template
+    # must multiply by sqrt(f) to plot template fft on top of ASD:
+    plt.figure(figsize=(10,6))
+    template_f = np.absolute(template_fft)*np.sqrt(np.abs(datafreq)) / d_eff
+    plt.loglog(datafreq, template_f, 'k', label='template(f)*sqrt(f)')
+    plt.loglog(freqs, np.sqrt(data_psd),pcolor, label=det+' ASD')
+    plt.xlim(20, fs/2)
+    plt.ylim(1e-24, 1e-20)
+    plt.grid()
+    plt.xlabel('frequency (Hz)')
+    plt.ylabel('strain noise ASD (strain/rtHz), template h(f)*rt(f)')
+    plt.legend(loc='upper left')
+    plt.title(det+' ASD and template around event')
+    plt.savefig('figures/'+eventname+"_"+det+"_matchfreq."+plottype)
